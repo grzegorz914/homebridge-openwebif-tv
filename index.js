0e2826753ad8caa56100c9882c8006af8c86ead2
Grzegorz <grzegorz914@icloud.com>
2020-04-19 09:36:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -462,12 +462,12 @@ class openwebIfTvDevice {
 		}
 		request(me.url + '/api/remotecontrol?command=' + command, function (error, response, data) {
 			if (error) {
-				me.log.debug('Device: %s can not send RC Command. Might be due to a wrong settings in config, error: %s', me.host, error);
+				me.log.debug('Device: %s can not setPowerModeSelection. Might be due to a wrong settings in config, error: %s', me.host, error);
 				callback(error);
 			} else {
-				me.log('Device: %s, setPowerModeSelection successfull, state: %s, command: %s', me.host, state ? 'HIDDEN' : 'SHOW', command);
-				me.currentInfoMenuState = !me.currentInfoMenuState;
-				callback(null, state);
+				me.log('Device: %s, setPowerModeSelection successfull, state: %s, command: %s', me.host, me.currentInfoMenuState ? 'HIDDEN' : 'SHOW', command);
+			        me.currentInfoMenuState = !me.currentInfoMenuState;
+		                callback(null, state);
 			}
 		});
 	}
@@ -485,10 +485,10 @@ class openwebIfTvDevice {
 		}
 		request(me.url + '/api/remotecontrol?command=' + command, function (error, response, data) {
 			if (error) {
-				me.log.debug('Device: %s can not send RC Command. Might be due to a wrong settings in config, error: %s', me.host, error);
+				me.log.debug('Device: %s can not send RC Command (Volume button). Might be due to a wrong settings in config, error: %s', me.host, error);
 				callback(error);
 			} else {
-				me.log('Device: %s, send RC Command successfull: %s', me.host, command);
+				me.log('Device: %s, send RC Command (Volume button) successfull, remoteKey: %s, command: %s', me.host, remoteKey, command);
 				callback(null);
 			}
 		});
