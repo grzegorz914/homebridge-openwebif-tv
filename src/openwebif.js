645aedb3227d5db9f88c8e759b11e249b26a81e6
Grzegorz <grzegorz914@icloud.com>
2022-10-15 21:07:40 +0200

    remove timeout

--- src/openwebif.js
+++ src/openwebif.js
@@ -22,7 +22,6 @@ class OPENWEBIF extends EventEmitter {
         this.axiosInstance = axios.create({
             method: 'GET',
             baseURL: url,
-            timeout: 10000,
             withCredentials: auth,
             auth: {
                 username: user,
