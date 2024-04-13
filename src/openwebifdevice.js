a0ef628786fb657a73b270e0b77b64642a4dc90d
Grzegorz <grzegorz914@icloud.com>
2024-04-13 16:50:10 +0200

    cleanup mqtt

--- src/openwebifdevice.js
+++ src/openwebifdevice.js
@@ -258,9 +258,7 @@ class OpenWebIfDevice extends EventEmitter {
                         .on('subscribed', (message) => {
                             this.emit('message', message);
                         })
-                        .on('subscribedMessage', async (data) => {
-                            const key = Object.keys(data)[0];
-                            const value = Object.values(data)[0];
+                        .on('subscribedMessage', async (key, value) => {
                             try {
                                 switch (key) {
                                     case 'Power':
